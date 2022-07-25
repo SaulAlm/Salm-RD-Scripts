ddfd35714c554847962ac256ed84eabe4a387fcc
saul-almeida <salmmotion@gmail.com>
2022-07-25 20:52:01 -0300

    'main'

--- Tools/IU_DefaultHierarchy.py
+++ Tools/IU_DefaultHierarchy.py
@@ -1,9 +1,20 @@
-# Ultra simple script to change the hierarchy folder for InvisibleUniverse studio.
+# Simple script to change the hierarchy folder for InvisibleUniverse studio.
+# version 1.01 - 2022.07.25
 
 from maya import cmds
 
+parentGroup = "FX_Grp"
+toParentGroup = 'Render_Grp'
+
 def parentFxGrp():
     if cmds.objExists('All_Grp'):
-        cmds.parent('FX_Grp', 'Render_Grp')
-        
-parentFxGrp()
+        cmds.parent(parentGroup, toParentGroup)
+
+# Search if exist some nodes in FX_Grp      
+def searchNodes():
+    someObj= cmds.listRelatives(parentGroup)
+    if not str(someObj) == 'None':
+        # Call the parent function
+        return parentFxGrp()
+
+searchNodes()
